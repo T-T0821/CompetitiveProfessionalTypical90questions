1505ba97b67362711063c96483e9b9792501785d
tsuno <tsuno@DESKTOP-IKF6GP6.localdomain>
2022-09-03 17:48:11 +0900

    001

--- python/001_YokanParty.py
+++ python/001_YokanParty.py
@@ -1,14 +1,37 @@
-# Input 2 integers
-
 import sys
 
-# Input two variables N, L
-N, L = map(int, input().split())
+def main(N, L, K, A):
+    left = -1
+    right = L + 1
+    while right - left > 1:
+        mid = (left + right) // 2
+        if is_ok(N, L, K, A, mid):
+            left = mid
+        else:
+            right = mid
 
-# Input one variables K
-K = int(input())
+    return left
 
-# Input n variables An
-A = list(map(int, input().split()))
+def is_ok(N, L, K, A, x):
+    count = 0
+    prev = 0
+    for a in A:
+        if a - prev >= x and L - a >= x:
+            count += 1
+            prev = a
 
+    return count >= K
 
+# main
+if __name__ == '__main__':
+    # Input two variables N, L
+    N, L = map(int, input().split())
+
+    # Input one variables K
+    K = int(input())
+
+    # Input n variables An
+    A = list(map(int, input().split()))
+
+    # Print N, L, K, A
+    print(main(N, L, K, A))
